e37572ba91a65fa6de4d28abd00486bd9d9f7b02
beatricecourage <bea_courage@hotmail.co.uk>
2019-07-17 13:00:48 +0100

    Returns fizz with  multiples of three

--- Tests/com/company/FizzbuzzTest.java
+++ Tests/com/company/FizzbuzzTest.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.Assert.*;
 
 public class FizzbuzzTest {
+
+    private Fizzbuzz fizzbuzz;
+
+
+    @Before
+    public void setup() {
+        fizzbuzz = new Fizzbuzz();
+
+    }
+
+    @DisplayName("Returns fizz with multiples of three")
+    @Test
+    public void fizz_3() {
+        String result = fizzbuzz.play(3);
+        assertEquals("fizz", result);
+    }
+
+
 
 
 
--- src/main/java/com/company/Fizzbuzz.java
+++ src/main/java/com/company/Fizzbuzz.java
@@ -2,14 +2,19 @@ package com.company;
 
 public class Fizzbuzz {
 
-
+    public String play(int number){
+        if (number % 3 == 0) {
+            return "fizz";
+        }
+        return null;
+    }
 
 
 
 }
 
 
-}
+
 
 //If divisible by 3, return fizz
 //if divisible by 5, return buzz
